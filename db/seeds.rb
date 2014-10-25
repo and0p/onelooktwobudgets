eea0f0b3302560854e98d1677d2b6b2104980939
Andrew Peterson <cybernetico@gmail.com>
2014-10-25 17:10:48 -0400

    Created a basic seeds.rb to create random items.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,45 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+max_int = 16777215
+
+item_list = [
+    "Shirt",
+    "Pants",
+    "Shoes",
+    "Tie",
+    "Hat"
+]
+
+color_list = [
+    "Blue",
+    "Red",
+    "Lavender",
+    "Pearl White",
+    "Joker Purple",
+    "Vomit Green",
+    "Burgandy"
+]
+
+description_list = [
+    "Ugly",
+    "Plaid",
+    "Double-breasted",
+    "Alien",
+    "Plate mail",
+    "+5 Int",
+    "Brave",
+    "Wool",
+    "Oxford",
+    "Silk"
+]
+
+for i in 1..300 do
+  Item.create( name: description_list.sample + " " + color_list.sample() + " " + item_list.sample(),
+               sku: "abc" + rand(50000).to_s,
+               msrp: rand(500),
+               color_hex: rand(max_int),
+               sale: false
+  )
+end
